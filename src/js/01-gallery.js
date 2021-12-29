bbba935e289f963a890b25cb4c9a184c5225d7c0
Dima Malik <mdima4266@gmail.com>
2021-12-29 16:06:20 +0200

    1 и 2 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+
+
+import { galleryItems } from './gallery-items.js';
+
+
+// // Change code below this line
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+                src="${preview}"
+                alt="${description}"    />
+        </a>`;
+    })
+    .join('');
+}
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const asd = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+asd.on("show.simplelightbox",function (e){})
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+const throttle = require("lodash.throttle");
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const _saveLocalTime = function _locTime(e) {
+
+localStorage.setItem('videoplayer-current-time', e.seconds);
+    console.log(e.seconds)
+
+    
+};
+player.on('timeupdate', throttle(_saveLocalTime, 2000))
+
+let curTime = localStorage.getItem('videoplayer-current-time');
+startValueTime();
+player.setCurrentTime(curTime).then(function (event) {
+    localStorage.setItem('videoplayer-current-time', curTime);
+});
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+});
+
+function startValueTime() {
+    localStorage.setItem('videoplayer-current-time', 0);
+}
+
 
